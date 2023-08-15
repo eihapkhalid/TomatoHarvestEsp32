0b7ad7ae01ef19d087dc067f9fa8c6e47c46066e
eihapKhalid <eihapinfo@gmail.com>
2023-08-15 16:15:38 +0300

    Sensor info

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,22 @@
 
 #include <Adafruit_MCP3008.h>
 
-/*wifi info*/
+// wifi info:
 const char* ssid = "abdo1987";
 const char* password = "abdo1987";
 const char* serverUrl = "http://localhost:7144/api/sensordata"; // server address
+
+// Sensor info:
+DHT dht[8]; // Create an array of 8 DHT sensors
+const int numLDR = 8; // The number of LDR sensors
+const int numSoilMoisture = 8; // Number of soil moisture sensors
+
+int ldrValues[numLDR];
+int soilMoistureValues[numSoilMoisture];
+
+float temperature[8];
+float humidity[8];
+
 
 void setup() {
 
