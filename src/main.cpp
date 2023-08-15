8906250682dfaf7414dae0a304d5b352802fc855
eihapKhalid <eihapinfo@gmail.com>
2023-08-15 16:10:27 +0300

    wifi info

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+
 #include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+
 #include "DHT_U.h"
+
 #include <Adafruit_MCP3008.h>
+
+/*wifi info*/
+const char* ssid = "abdo1987";
+const char* password = "abdo1987";
+const char* serverUrl = "http://localhost:7144/api/sensordata"; // server address
 
 void setup() {
 
