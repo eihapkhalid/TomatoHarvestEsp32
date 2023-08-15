436d471cfbfba5bcfcad7e2e1362062c5320e0b6
eihapKhalid <eihapinfo@gmail.com>
2023-08-15 16:53:09 +0300

    initialization

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <HTTPClient.h>
 
 #include "DHT_U.h"
-
+#include <DHTesp.h>
 #include <Adafruit_MCP3008.h>
 
 // wifi info:
@@ -14,7 +14,7 @@ const char* password = "abdo1987";
 const char* serverUrl = "http://localhost:7144/api/sensordata"; // server address
 
 // Sensor info:
-DHT dht[8]; // Create an array of 8 DHT sensors
+DHTesp dht[8]; // Create an array of 8 DHT sensors
 const int numLDR = 8; // The number of LDR sensors
 const int numSoilMoisture = 8; // Number of soil moisture sensors
 
@@ -25,8 +25,24 @@ float temperature[8];
 float humidity[8];
 
 Adafruit_MCP3008 adc; // Create an instance of MCP3008
-void setup() {
 
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to Wi-Fi...");
+  }
+
+  // DHT22 sensors initialization
+  for (int i = 0; i < 8; i++) {
+    dht[i].setup(4 + i, DHTesp::DHT22);
+  }
+  
+  adc.begin(); // MCP3008 initialization
+
+  Serial.println("Wi-Fi is connected");
 }
 
 void loop() {
